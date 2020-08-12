2ffdb853657841078b4945fe8637591597b7bb16
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2020-08-12 16:38:45 +0100

    autocomplete, view components, viewmodels, utils

--- app/controllers/register/LogoutController.scala
+++ app/controllers/register/LogoutController.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@(headingKey: String, optionalParam: Option[String] = None, headingSize: String = "heading-large")(implicit messages: Messages)
+package controllers.register
 
-<h1 class="@headingSize">@messages(headingKey, if(optionalParam.isDefined) {optionalParam.get} else "")</h1>
+import com.google.inject.{Inject, Singleton}
+import config.FrontendAppConfig
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+
+@Singleton
+class LogoutController @Inject()(appConfig: FrontendAppConfig, val controllerComponents: MessagesControllerComponents) extends FrontendBaseController {
+
+  def logout: Action[AnyContent] = Action {
+    implicit request =>
+      Redirect(appConfig.logoutUrl).withNewSession
+  }
+}

fcdfaa8b06d43e4db010954feaa1602ef268be94
Adam Conder <adamdconder@gmail.com>
2021-01-28 12:16:36 +0000

    Add busines protector residency controllers and specs

---
+++ app/controllers/register/business/mld5/CountryOfResidenceYesNoController.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.business.mld5
+
+import config.annotations.BusinessProtector
+import controllers.actions._
+import controllers.actions.register.business.NameRequiredAction
+import forms.YesNoFormProvider
+import javax.inject.Inject
+import navigation.Navigator
+import pages.register.business.mld5.CountryOfResidenceYesNoPage
+import play.api.data.Form
+import play.api.i18n._
+import play.api.mvc._
+import repositories.RegistrationsRepository
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.register.business.mld5.CountryOfResidenceYesNoView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class CountryOfResidenceYesNoController @Inject()(
+                                                   val controllerComponents: MessagesControllerComponents,
+                                                   repository: RegistrationsRepository,
+                                                   @BusinessProtector navigator: Navigator,
+                                                   standardActionSets: StandardActionSets,
+                                                   nameAction: NameRequiredAction,
+                                                   formProvider: YesNoFormProvider,
+                                                   view: CountryOfResidenceYesNoView
+                                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form: Form[Boolean] = formProvider.withPrefix("businessProtector.5mld.countryOfResidenceYesNo")
+
+  def onPageLoad(index: Int, draftId: String): Action[AnyContent] =
+    standardActionSets.identifiedUserWithData(draftId).andThen(nameAction(index)) {
+      implicit request =>
+
+        val preparedForm = request.userAnswers.get(CountryOfResidenceYesNoPage(index)) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
+
+        Ok(view(preparedForm, draftId, index, request.protectorName))
+    }
+
+  def onSubmit(index: Int, draftId: String): Action[AnyContent] =
+    standardActionSets.identifiedUserWithData(draftId).andThen(nameAction(index)).async {
+      implicit request =>
+
+        form.bindFromRequest().fold(
+          formWithErrors =>
+            Future.successful(BadRequest(view(formWithErrors, draftId, index, request.protectorName))),
+
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(CountryOfResidenceYesNoPage(index), value))
+              _              <- repository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(CountryOfResidenceYesNoPage(index), draftId, updatedAnswers))
+        )
+    }
+}

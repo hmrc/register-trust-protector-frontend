6fbfaaa93b4a52097c2eeee3487383a7fa761f31
Gaston Racui <gastonracui@gmail.com>
2023-04-12 08:33:22 +0100

    DDCE-3925 Upgrade dependencies to build jdk 11 and scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.14.0"
+  val bootstrapVersion = "7.15.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -18,13 +18,13 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"                % "3.2.15",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
-    "org.jsoup"              %  "jsoup"                    % "1.15.3",
+    "org.jsoup"              %  "jsoup"                    % "1.15.4",
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current,
     "org.mockito"            %% "mockito-scala-scalatest"  % "1.17.12",
     "org.scalacheck"         %% "scalacheck"               % "1.17.0",
     "wolfendale"             %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.github.tomakehurst" %  "wiremock-standalone"      % "2.27.2",
-    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.62.2"
+    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.64.0"
 
   ).map(_ % Test)
 
